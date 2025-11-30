9ce11b13c850c45a160fd407360675b4afceceb4
nirbhaynikam <nikamnirbhay9@gmail.com>
2025-11-30 13:44:49 +0530

    Anshari changes

---
+++ js/config.js
@@ -0,0 +1,69 @@
+// Configuration for all external links and file locations
+const config = {
+    // Education Results
+    education: {
+        bedResults: 'https://drive.google.com/drive/folders/1qYFkwmh_pRYexXaS8jU0yxnkRgWbqPeJ?usp=drive_link',
+        bscResults: 'https://drive.google.com/drive/folders/1YaLw5m7PF7jVmXayYhhPYDoPxtIe4uUO?usp=drive_link',
+        hscResults: 'https://drive.google.com/drive/folders/1adh0wfudO3ysQ09DtgKcMduiBtnsGfap?usp=drive_link',
+        sscResults: 'https://drive.google.com/drive/folders/1MjNPQLn7zPyZiE-lY_zGqBeEI8bxsz-9?usp=drive_link'
+    },
+    
+    // Certifications
+    certifications: {
+        driveLink: 'https://drive.google.com/drive/folders/1DDYL9raYVbi4jtakb9f2mQKBfeguirTa?usp=drive_link'
+    },
+    
+    // Research Work
+    research: {
+        driveLink: 'https://drive.google.com/drive/folders/1KgQ-JwS89UjGpyYNdTnbMRBTmqjgiWko?usp=drive_link' // Add your research work drive link here
+    },
+    
+    // Community Service
+    communityService: {
+        driveLink: 'https://drive.google.com/file/d/1Oi1e2bc6KQYx7uuaQt7TLvMZhce3M_QA/view?usp=drive_link' // Add your community service drive link here
+    },
+    
+    // Resume and Profile
+    resume: {
+        file: 'Devyani_Nikam_Resume.pdf',
+        path: '/Devyani_Nikam_Resume.pdf'
+    },
+    
+    // Images
+    images: {
+        profile: 'images/1.jpeg',
+        logo: 'images/urja-trust-logo.png'
+    },
+    
+    // Social & Contact
+    social: {
+        linkedin: 'https://www.linkedin.com/in/devyani-nikam-a80914392',
+        email: 'devyaninikambttc@gmail.com',
+        phone: '+91 86004 40323'
+    },
+    
+    // Gallery/Highlights
+    highlights: {
+        images: [
+            'images/1.jpeg',
+            'images/2.jpeg',
+            'images/3.jpeg',
+            'images/4.jpeg',
+            'images/5.jpeg',
+            'images/6.jpeg',
+            'images/7.jpeg',
+            'images/8.jpeg',
+            'images/9.jpeg',
+            'images/10.jpeg',
+            'images/11.jpeg',
+            'images/12.jpeg',
+            'images/13.jpeg',
+            'images/14.jpeg',
+            'images/15.jpeg',
+            'images/16.jpeg',
+            'images/17.jpeg',
+            'images/18.jpeg',
+            'images/19.jpeg'
+        ]
+    }
+};
